165fa244c678819bedc588047bc10f447356463d
akash-gupta-146 <ag.akash007@gmail.com>
2018-01-27 13:05:16 +0530

    Updated animations

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,11 +3,28 @@ import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  host: {
+    '(window:scroll)': 'updateHeader($event)'
+}
 })
 
 export class HeaderComponent{
 
+  isScrolled = false;
+  currPos: Number = 0;
+  startPos: Number = 0;
+  changePos: Number = 100;
+
   constructor() { }
+
+  updateHeader(evt) {
+    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+    if(this.currPos >= this.changePos ) {
+        this.isScrolled = true;
+    } else {
+        this.isScrolled = false;
+    }
+}
 
 }
