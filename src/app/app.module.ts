4205b15998ee65f67e254336ca05edb03446401d
akash-gupta-146 <ag.akash007@gmail.com>
2018-02-02 13:42:34 +0530

    Fixed bugs for website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AgmCoreModule } from '@agm/core';
 import { BlockHeadingComponent } from './block-heading/block-heading.component';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { ScreenshotsComponent } from './screenshots/screenshots.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
     NxtlifeServicesComponent,
     AboutComponent,
     ContactComponent,
-    BlockHeadingComponent
+    BlockHeadingComponent,
+    ScreenshotsComponent
   ],
   imports: [
     BrowserModule,
