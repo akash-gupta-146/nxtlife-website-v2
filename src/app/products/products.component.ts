8b599a912f6cfdaf34ea3a14258054259b93b1cb
akash-gupta-146 <ag.akash007@gmail.com>
2018-02-02 13:39:55 +0530

    Added loop in products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,12 +8,61 @@ import { Block } from '../block-heading/block-heading.component';
 })
 export class ProductsComponent implements OnInit {
   heading : Block;
-  
+  products: Product[];
     constructor() {
       this.heading = { title: 'Products', paragraph: `There are many variations of lorem of Lorem Ipsum available for use a sit amet, consectetur debits adipisicing lacus.There
-      are many variations of lorem of Lorem Ipsum available for use a sit amet, consectetur debits adipisicing lacus`}
+      are many variations of lorem of Lorem Ipsum available for use a sit amet, consectetur debits adipisicing lacus`
+      }
+
+      this.products = [
+        {
+          name:'Strategic Planning',
+          description:'Links Today With Tomorrow',
+          icon:'',
+          route:''
+        },
+        {
+          name:"Stakeholders' Experience Management",
+          description:"Design and Manage Stakeholder' Journey" ,
+          icon:'',
+          route:''
+        },
+
+        {
+          name:'Rish Management & Internal Audidt',
+          description:'consectetur adipiscing elit. Etiam posuere urna in suscipit tempor',
+          icon:'',
+          route:''
+        },
+        {
+          name:'Quality Assurance & Accreditation',
+          description:'consectetur adipiscing elit. Etiam posuere urna in suscipit tempor',
+          icon:'',
+          route:''
+        },
+        {
+          name:'Learning Experience & Management',
+          description:'consectetur adipiscing elit. Etiam posuere urna in suscipit tempor',
+          icon:'',
+          route:''
+        },
+        {
+          name:'International University Ranking',
+          description:'consectetur adipiscing elit. Etiam posuere urna in suscipit tempor',
+          icon:'',
+          route:''
+        },        
+      ]
+
+
      }
   ngOnInit() {
   }
 
+}
+export class Product{
+  name: string;
+  description: string;
+  icon: string;
+  route:string;
 }
